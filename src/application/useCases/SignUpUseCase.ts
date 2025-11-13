e578b055937e93b0579656cf36bd7561f172f878
michelzaum <michel.oliveira999@hotmail.com>
2025-11-12 22:52:45 -0300

    feat: implement SignUpUseCase logic

--- src/application/useCases/SignUpUseCase.ts
+++ src/application/useCases/SignUpUseCase.ts
@@ -1,3 +1,20 @@
+import { prismaClient } from '../../lib/prismaClient';
+
+interface IInput {
+  name: string;
+  email: string;
+  password: string;
+}
+
 export class SignUpUseCase {
-  execute() {}
+  async execute({ name, email, password }: IInput) {
+    await prismaClient.user.create({
+      data: {
+        name,
+        email,
+        password,
+        role: 'USER',
+      },
+    });
+  }
 }
