b378d5165911739d667a48edb229b71d95f38d16
michelzaum <michel.oliveira999@hotmail.com>
2025-11-13 22:01:49 -0300

    refactor: move body information destructuring inside try/catch block

--- src/application/controllers/SignUpController.ts
+++ src/application/controllers/SignUpController.ts
@@ -5,15 +5,15 @@ export class SignUpController implements IController{
   constructor(private readonly signUpUseCase: SignUpUseCase) {}
 
   async handler({ body }: IRequest): Promise<IResponse> {
-    const { name, email, password } = body;
-
     try {
+      const { name, email, password } = body;
+
       await this.signUpUseCase.execute({ name, email, password });
 
       return {
         statusCode: 204,
         body: null,
-      }
+      };
     } catch (error) {
       console.log(error);
       return {
