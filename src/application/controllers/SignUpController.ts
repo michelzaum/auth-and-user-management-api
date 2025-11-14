f41733cd16953ff9ccd7f66ae946eceb889cd6b7
michelzaum <michel.oliveira999@hotmail.com>
2025-11-13 22:09:17 -0300

    feat: ensure received body in request is valid

--- src/application/controllers/SignUpController.ts
+++ src/application/controllers/SignUpController.ts
@@ -1,12 +1,20 @@
+import z from "zod";
+
 import { IController, IRequest, IResponse } from "../interfaces/IController";
 import { SignUpUseCase } from "../useCases/SignUpUseCase";
+
+const schema = z.object({
+  name: z.string().min(1),
+  email: z.email().min(1),
+  password: z.string().min(8),
+});
 
 export class SignUpController implements IController{
   constructor(private readonly signUpUseCase: SignUpUseCase) {}
 
   async handler({ body }: IRequest): Promise<IResponse> {
     try {
-      const { name, email, password } = body;
+      const { email, name, password } = schema.parse(body);
 
       await this.signUpUseCase.execute({ name, email, password });
 
