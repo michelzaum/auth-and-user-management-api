a5387bd143a6baf208c499e2839d6f0e0e125671
michelzaum <michel.oliveira999@hotmail.com>
2025-11-19 23:16:00 -0300

    feat: create DeleteUserController

---
+++ src/application/controllers/DeleteUserController.ts
@@ -0,0 +1,17 @@
+import { IController, IRequest, IResponse } from "../interfaces/IController";
+import { DeleteUserUseCase } from "../useCases/DeleteUserUseCase";
+
+export class DeleteUserController implements IController {
+  constructor(private readonly deleteUserUseCase: DeleteUserUseCase) {}
+
+  async handler(request: IRequest): Promise<IResponse> {
+    const { id } = request.params;
+
+    const result = this.deleteUserUseCase.execute(id);
+
+    return {
+      statusCode: 200,
+      body: result,
+    }
+  }
+}
