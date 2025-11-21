bc1299dc752bce28703efd55c6cf89d9533251f9
michelzaum <michel.oliveira999@hotmail.com>
2025-11-20 22:44:01 -0300

    feat: check if e-mail is already in database before creating a new user

--- src/application/useCases/SignUpUseCase.ts
+++ src/application/useCases/SignUpUseCase.ts
@@ -1,4 +1,5 @@
 import { prismaClient } from '../../lib/prismaClient';
+import { UserAlreadyExists } from '../errors/UserAlreadyExists';
 
 interface IInput {
   name: string;
@@ -8,6 +9,14 @@ interface IInput {
 
 export class SignUpUseCase {
   async execute({ name, email, password }: IInput) {
+    const userAlreadyExists = await prismaClient.user.findUnique({
+      where: { email },
+    });
+
+    if (userAlreadyExists) {
+      throw new UserAlreadyExists();
+    }
+
     await prismaClient.user.create({
       data: {
         name,
