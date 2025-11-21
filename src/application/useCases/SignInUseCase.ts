21019c5832b095d9b51b9c97f7c07d721b3ba5a7
michelzaum <michel.oliveira999@hotmail.com>
2025-11-20 23:26:20 -0300

    feat: create SignInUseCase and its implementation

---
+++ src/application/useCases/SignInUseCase.ts
@@ -0,0 +1,32 @@
+import { compare } from 'bcrypt';
+import { sign } from 'jsonwebtoken';
+
+import { prismaClient } from "../../lib/prismaClient";
+import { InvalidCredentials } from "../errors/InvalidCredentials";
+import { config } from '../config/env';
+
+export class SignInUseCase {
+  async execute(email: string, password: string) {
+    const user = await prismaClient.user.findUnique({
+      where: { email },
+    });
+
+    if (!user) {
+      throw new InvalidCredentials();
+    }
+
+    const isPasswordValid = await compare(password, user.password);
+
+    if (!isPasswordValid) {
+      throw new InvalidCredentials();
+    }
+
+    const accessToken = sign(
+      { sub: user.id },
+      config.jwtSecret,
+      { expiresIn: '1d' },
+    );
+
+    return accessToken;
+  }
+}
