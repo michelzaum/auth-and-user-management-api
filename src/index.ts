10fcb98a01bd90de255e74e3699b83b991810b5b
michelzaum <michel.oliveira999@hotmail.com>
2025-11-22 16:52:09 -0300

    feat: create makeGetLoggedUserController

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 
-import { makeSignUpController } from './factories/makeSignUpController';
 import { routeAdapter } from './adapters/routeAdapter';
+
+import { makeSignUpController } from './factories/makeSignUpController';
 import { makeListAllUsersController } from './factories/makeListAllUsersController';
 import { makeUpdateUserController } from './factories/makeUpdateUserController';
 import { makeDeleteUserController } from './factories/makeDeleteUserController';
 import { makeSignInController } from './factories/makeSignInController';
+import { makeGetLoggedUserController } from './factories/makeGetLoggedUserController';
 
 const app = express();
 const port = 3001;
@@ -17,6 +19,7 @@ app.get('/users', routeAdapter(makeListAllUsersController()));
 app.put('/users/:id', routeAdapter(makeUpdateUserController()));
 app.delete('/users/:id', routeAdapter(makeDeleteUserController()));
 app.post('/sign-in', routeAdapter(makeSignInController()));
+app.get('/me', routeAdapter(makeGetLoggedUserController()));
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
