8e276aba101ee0ff2d295b41a5538b66dd1e5cbd
michelzaum <michel.oliveira999@hotmail.com>
2025-12-03 00:14:45 -0300

    feat: implement authorization token validation in AuthenticationMiddleware

--- src/application/middlewares/AuthenticationMiddleware.ts
+++ src/application/middlewares/AuthenticationMiddleware.ts
@@ -1,13 +1,45 @@
-import { IMiddleware, IResponse } from "../interfaces/IMiddleware";
+import { JwtPayload, verify } from "jsonwebtoken";
+import { IData, IMiddleware, IResponse } from "../interfaces/IMiddleware";
 import { IRequest } from "../interfaces/IRequest";
+import { env } from "../config/env";
 
 export class AuthenticationMiddleware implements IMiddleware {
-  async handle({ headers }: IRequest): Promise<IResponse> {
-    console.log(headers);
+  async handle({ headers }: IRequest): Promise<IResponse | IData> {
+    const authorization = headers.authorization;
 
-    return {
-      statusCode: 200,
-      body: {},
+    if (!authorization) {
+      return {
+        statusCode: 401,
+        body: {
+          message: 'Unhautorized',
+        }
+      }
+    }
+
+    try {
+      const [bearer, token] = authorization.split(' ');
+
+      if (bearer != 'Bearer') {
+        throw new Error();
+      }
+
+      const payload = verify(token, env.jwtSecret) as JwtPayload;
+
+      return {
+        data: {
+          user: {
+            id: payload.id,
+            role: payload.role,
+          }
+        }
+      }
+    } catch (error) {
+      return {
+        statusCode: 400,
+        body: {
+          message: 'Invalid access token',
+        }
+      }
     }
   }
 }
