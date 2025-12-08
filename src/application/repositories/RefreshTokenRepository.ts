41ea7ac43bbd90e042df8cbb0a62b6e9c1b48a5b
michelzaum <michel.oliveira999@hotmail.com>
2025-12-08 17:04:38 -0300

    feat: add 'create' method in RefreshTokenRepository class

--- src/application/repositories/RefreshTokenRepository.ts
+++ src/application/repositories/RefreshTokenRepository.ts
@@ -1,9 +1,22 @@
 import { prismaClient } from '../../lib/prismaClient';
 
+interface ICreateDTO {
+  userId: string;
+  expiresAt: Date;
+}
+
 export class RefreshTokenRepository {
   async findById(id: string) {
     return prismaClient.refreshToken.findUnique({
       where: { id },
+    });
+  }
+
+  async create({ userId, expiresAt }: ICreateDTO) {
+    return prismaClient.refreshToken.create({
+      data: {
+        userId, expiresAt,
+      }
     });
   }
 }
