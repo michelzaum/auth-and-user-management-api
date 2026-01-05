cd9ee4a8658843495fbfc7483b3c5e6e37d86c54
michelzaum <michel.oliveira999@hotmail.com>
2026-01-04 23:25:29 -0300

    feat: validate user role in AuthorizationMiddleware

--- src/application/middlewares/AuthorizationMiddleware.ts
+++ src/application/middlewares/AuthorizationMiddleware.ts
@@ -1,15 +1,53 @@
+import { JwtPayload, verify } from "jsonwebtoken";
+
 import { IData, IMiddleware, IResponse } from "../interfaces/IMiddleware";
 import { IRequest } from "../interfaces/IRequest";
+import { env } from "../config/env";
 
 export class AuthorizationMiddleware implements IMiddleware {
-  constructor(allowedRoles: string[]) {
-    console.log(allowedRoles)
-  }
+  constructor(private readonly allowedRoles: string[]) {}
 
   async handle(request: IRequest): Promise<IResponse | IData> {
+    const accessToken = request.headers.authorization;
+
+    if (!accessToken) {
+      return {
+        statusCode: 401,
+        body: {
+          error: 'Unauthorized',
+        }
+      }
+    }
+
+    const [bearer, token] = accessToken.split(' ');
+
+    if (bearer !== 'Bearer') {
+      return {
+        statusCode: 400,
+        body: {
+          error: 'Invalid access token',
+        },
+      }
+    }
+
+    const payload = verify(token, env.jwtSecret) as JwtPayload;
+
+    if (!this.allowedRoles.includes(payload.role)) {
+      return {
+        statusCode: 403,
+        body: {
+          error: 'Forbidden',
+        },
+      }
+    }
+
     return {
-      statusCode: 200,
-      body: {}
+      data: {
+        user: {
+          id: payload.sub,
+          role: payload.role,
+        }
+      }
     }
   }
 }
