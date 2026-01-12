137269af1dc785a672d7e6c87b1389bec3f0183c
michelzaum <michel.oliveira999@hotmail.com>
2026-01-11 22:06:47 -0300

    feat: use try / catch block in UpdateLoggedUserController class

--- src/application/controllers/UpdateLoggedUserController.ts
+++ src/application/controllers/UpdateLoggedUserController.ts
@@ -10,41 +10,45 @@ export class UpdateLoggedUserController implements IController {
   constructor(private readonly updateLoggedUserUseCase: UpdateLoggedUserUseCase) {}
 
   async handler(request: IRequest): Promise<IResponse> {
-    const authorization = request.headers.authorization;
-
-    if (!authorization) {
-      const { name, httpCode, isOperational, message } = new Unauthorized();
-
-      throw new AppError(
-        name,
-        httpCode,
-        isOperational,
-        message,
-      );
-    }
-
-    const { name, email, password } = request.body;
-
-    const [, token] = authorization.split(' ');
-
-    const { sub: userId } = verify(token, env.jwtSecret) as JwtPayload;
-
-    if (!userId) {
+    try {
+      const authorization = request.headers.authorization;
+  
+      if (!authorization) {
+        const { name, httpCode, isOperational, message } = new Unauthorized();
+  
+        throw new AppError(
+          name,
+          httpCode,
+          isOperational,
+          message,
+        );
+      }
+  
+      const { name, email, password } = request.body;
+  
+      const [, token] = authorization.split(' ');
+  
+      const { sub: userId } = verify(token, env.jwtSecret) as JwtPayload;
+  
+      if (!userId) {
+        return {
+          statusCode: 400,
+          body: {
+            error: 'Invalid userId',
+          },
+        }
+      }
+  
+      const updatedUser = await this.updateLoggedUserUseCase.execute(userId, { name, email, password })
+  
       return {
-        statusCode: 400,
+        statusCode: 200,
         body: {
-          error: 'Invalid userId',
-        },
+          user: updatedUser,
+        }
       }
-    }
-
-    const updatedUser = await this.updateLoggedUserUseCase.execute(userId, { name, email, password })
-
-    return {
-      statusCode: 200,
-      body: {
-        user: updatedUser,
-      }
+    } catch (error) {
+      throw error;
     }
   }
 }
