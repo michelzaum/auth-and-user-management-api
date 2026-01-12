63499fb454a07184657327122650350d6a091412
michelzaum <michel.oliveira999@hotmail.com>
2026-01-11 22:08:11 -0300

    feat: use try / catch block in UpdateUserController class

--- src/application/controllers/UpdateUserController.ts
+++ src/application/controllers/UpdateUserController.ts
@@ -6,14 +6,18 @@ export class UpdateUserController implements IController {
   constructor(private readonly updateUserUseCase: UpdateUserUseCase) {}
 
   async handler(request: IRequest): Promise<IResponse> {
-    const { name, email, password, role } = request.body;
-    const { id } = request.params;
-
-    const result = await this.updateUserUseCase.execute({ id, name, email, password, role });
-
-    return {
-      statusCode: 200,
-      body: result,
+    try {
+      const { name, email, password, role } = request.body;
+      const { id } = request.params;
+  
+      const result = await this.updateUserUseCase.execute({ id, name, email, password, role });
+  
+      return {
+        statusCode: 200,
+        body: result,
+      }
+    } catch (error) {
+      throw error;
     }
   }
 }
