6e326dfae1ebd46521c9e1fe7cb988e9696faaa5
michelzaum <michel.oliveira999@hotmail.com>
2026-01-18 22:23:23 -0300

    feat: use transaction to delete user in delete user by admin feature

--- src/application/useCases/DeleteUserUseCase.ts
+++ src/application/useCases/DeleteUserUseCase.ts
@@ -1,9 +1,26 @@
 import { prismaClient } from "../../lib/prismaClient";
+import { HttpCodes } from "../../lib/shared/httpCodes";
+import { AppError } from "../errors/AppError";
 
 export class DeleteUserUseCase {
   async execute(id: string) {
-    return await prismaClient.user.delete({
-      where: { id },
+    await prismaClient.$transaction(async (tx) => {
+      const userToDelete = await tx.user.findUnique({
+        where: { id },
+      });
+
+      if (!userToDelete) {
+        throw new AppError(
+          'User not found',
+          HttpCodes.BadRequest,
+          true,
+          `User with id ${id} not found.`,
+        );
+      }
+
+      return await tx.user.delete({
+        where: { id },
+      });
     });
   }
 }
